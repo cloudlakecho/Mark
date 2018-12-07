13f10a65add0c12059f9463d7d011cf82e2e5985
cloudlakecho <cloudlakecho@yahoo.com>
2018-12-06 22:26:44 -0800

    Comment modified

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 
-# main.py Perfomr PCA
-# Cloud Cho May 26, 2018
+# main.py in Anthony-Stockholm 
+# Cloud Cho May 26, 2018 - Perfomr PCA
 #
-# error exist
+# Error:
+#   error exist
+# 
+# Comment:
+#
 
 import numpy as np
 import sklearn
 from sklearn.decomposition import PCA
 
-print(sklearn.__version__)
+import argparse
+import keras
+import os.path
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-pca = PCA(n_components=2)
-pca.fit(X)
-print(pca.explained_variance_ratio_)  
-# Error
-# Cause: version under 0.19
-#print(pca.singular_values_)  
+from pathlib2 import Path #  pipy library
+from inspect import currentframe, getframeinfo
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Flatten, Conv2D
 
-pca = PCA(n_components=2, svd_solver='full')
-pca.fit(X)                 
-print(pca.explained_variance_ratio_)  
-#print(pca.singular_values_)  
 
-pca = PCA(n_components=1, svd_solver='arpack')
-pca.fit(X)
-print(pca.explained_variance_ratio_)  
-#print(pca.singular_values_)  
+def main():
+    X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    pca = PCA(n_components=2)
+    pca.fit(X)
+    print(pca.explained_variance_ratio_)  
+    
+    # Error spot
+    # Cause: version under 0.19
+    #print(pca.singular_values_)  
 
+    pca = PCA(n_components=2, svd_solver='full')
+    pca.fit(X)                 
+    print(pca.explained_variance_ratio_)  
+    #print(pca.singular_values_)  
+
+    pca = PCA(n_components=1, svd_solver='arpack')
+    pca.fit(X)
+    print(pca.explained_variance_ratio_)  
+    #print(pca.singular_values_)  
+
+
+if __name__ == '__main__':
+    print(sklearn.__version__)
+    main()
+    
