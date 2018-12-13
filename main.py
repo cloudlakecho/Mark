cffcd4c0d6e067ac03654d1f0d6b44b2fbe8acd6
cloudlakecho <cloudlakecho@users.noreply.github.com>
2018-12-13 11:52:44 -0800

    Minor modification

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from sklearn.decomposition import PCA
 
 import argparse
 import keras
+print("Keras impoted")
 import os.path
 
 from pathlib2 import Path #  pipy library
@@ -21,11 +22,18 @@ from inspect import currentframe, getframeinfo
 from keras.models import Sequential
 from keras.layers import Activation, Dense, Flatten, Conv2D
 
+from tool import lstm
 
+
+#
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+#
 def main():
-    pass
+	file_name = raw_input("Please, give input text file, thanks.")
+	data_manipulation(file_name)
+	make_dataset()
+	train_model()
 
 
 if __name__ == '__main__':
     main()
-    
